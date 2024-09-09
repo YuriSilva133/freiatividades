e4f6c0f8ada86b323ffc7a97f40188b621fa977d
Your Name <you@example.com>
2024-09-09 16:06:14 -0300

    switch, Lista de Objetos, props

--- src/pages/app/index.jsx
+++ src/pages/app/index.jsx
@@ -4,7 +4,6 @@ import Cabecalho from '../../components/cabecalho'
 
 export default function App() {
 
-    
     return (
         <div className='pagina-app'>
             <Cabecalho />
@@ -12,96 +11,164 @@ export default function App() {
             <div className='secao'>
                 <h1 className='titulo'> Conteúdos </h1>
 
+
                 <div className='cards'>
-                    <Card 
-                        status = 'completo'
-                        titulo = 'Componentes'
-                        assunto = 'Assuntos'
-                        descricao = '- Objetos e Listas de Objetos - Componentes'
-                        data = '26/agosto' 
-                        entregar= 'não'
-                    />
 
-                    <Card 
-                        status = 'em andamento'
-                        titulo = 'Renderização e Efeito'
-                        assunto = 'Assuntos'
-                        descricao = '- Componentes na renderização.'
-                        data = '2/setembro' 
-                        entregar= 'não'
-                    />
+                    <Card />
 
-                    <Card 
-                        status = 'criado'
-                        titulo = 'Consumindo APIs - Parte 1'
-                        assunto = 'Assuntos'
-                        descricao = '- Componentes na renderização.'
-                        data = '9/setembro' 
-                        entregar= 'sim' 
-                    />
 
-                    <Card 
-                        status = 'evento'
-                        titulo = 'Componentes'
-                        assunto = 'Assuntos'
-                        descricao = '- Objetos e Listas de Objetos - Componentes'
-                        data = '16/setembro' 
-                        entregar= 'não'
-                    />
+
+                    {/*
+                    <Card
+                        status='completo'
+                        titulo='Componentes'
+                        descricao='- Objetos e Listas de Objetos - Componentes'
+                        data='26/agosto'
+                        trabalho={false}
+                    /> 
                     
-                    <Card/>
+                    <Card
+                        status='em andamento'
+                        titulo='Renderização e Efeito'
+                        descricao='- Componentes na renderização.'
+                        data='2/setembro'
+                        trabalho={false}
+                    />
+
+                    <Card
+                        status='criado'
+                        titulo='Consumindo APIs - Parte 1'
+                        descricao='- Componentes na renderização.'
+                        data='9/setembro'
+                        trabalho={true}
+                    />
+
+                    <Card
+                        status='evento'
+                        titulo='Componentes'
+                        descricao='- Objetos e Listas de Objetos - Componentes'
+                        data='16/setembro'
+                        trabalho={false}
+                    /> 
+                    */}
                 </div>
             </div>
         </div>
     )
-
 }
 
-export function Card(props) {
-    let corPrimaria = ''
-    let corSegundaria = ''
+export function Card({ titulo, status, descricao, data, trabalho }) {
 
-    if (props.status === 'completo') {
+    let corPrimaria = '#A3E5BA'
+    let corSegundaria = '#EFFBE2'
+
+    let listaObjetos = [
+        {
+            "status": "completo",
+            "titulo": "Componentes",
+            "descricao": "- Objetos e Listas de Objetos",
+            "data": "02/fevereiro",
+            "trabalho": true,
+            "fimBimestre": false
+        }
+    ]
+
+    switch (status) {
+        case 'completo':
+            corPrimaria = '#A3E5BA'
+            corSegundaria = '#EFFBE2'
+            break;
+
+        case 'em andamento':
+            corPrimaria = '#DCA3E5'
+            corSegundaria = '#EDE2FB'
+            break;
+
+        case 'evento':
+            corPrimaria = '#F6E448'
+            corSegundaria = '#FAF4C1'
+            break
+
+        case 'criado':
+            corPrimaria = '#C3C3C3'
+            corSegundaria = '#F2F2F2'
+            break
+
+        default:
+            //corSegundaria = '#F2F2F2'
+            break;
+    }
+
+    /*
+    if (status === 'completo') {
         corPrimaria = '#A3E5BA'
         corSegundaria = '#EFFBE2'
     }
-    else if (props.status === 'em andamento') {
+    else if (status === 'em andamento') {
         corPrimaria = '#DCA3E5'
         corSegundaria = '#EDE2FB'
     }
-    else if (props.status === 'criado') {
+    else if (status === 'criado') {
         corPrimaria = '#C3C3C3'
         corSegundaria = '#F2F2F2'
     }
-    else if (props.status === 'evento') {
+    else if (status === 'evento') {
         corPrimaria = '#F6E448'
         corSegundaria = '#FAF4C1'
     }
-    
+    */
+
     return (
         <div className='cards'>
+
+            {/* 
             <div className='card' style={{ backgroundColor: corSegundaria }}>
                 <div className='card-cabecalho' style={{ backgroundColor: corPrimaria }}>
-                    <h2>{props.titulo}</h2>
+                    <h2>{titulo}</h2>
                 </div>
                 <div className='card-conteudo'>
-                    <h3>{props.assunto}</h3>
 
-                    <pre style={{ whiteSpace: 'pre-line' }}>
-                        {props.descricao}
-                    </pre>
+                    {descricao &&
+                        <div>
+                            <h3>Assuntos</h3>
+                            <pre>
+                                {descricao}
+                            </pre>
+                        </div>
+                    }
                 </div>
 
                 <div className='card-tags'>
-                    {props.data &&
-                        <p className='tag'>{props.data}</p>
-                    }
-
-                    {props.entregar === 'sim' &&
-                        <p className='entregar'> Entrega Trab.</p>
-                    }
+                    {data && <p className='tag'> {data} </p>}
+                    {trabalho && <p className='entregar'> Entrega Trab.</p>}
                 </div>
-            </div>
+            </div> 
+            */}
+
+            {listaObjetos.map((item) => (
+                <div className='card' style={{ backgroundColor: corSegundaria}}>
+                    <div className='card-cabecalho' style={{ backgroundColor: corPrimaria }}>
+
+                        <h2>{item.titulo}</h2>
+                    </div>
+                    <div className='card-conteudo'>
+
+                        {item.descricao &&
+                            <div>
+                                <h3>Assuntos</h3>
+                                <pre>
+                                    {item.descricao}
+                                </pre>
+                            </div>
+                        }
+                    </div>
+
+                    <div className='card-tags'>
+                        {item.data && <p className='tag'> {item.data} </p>}
+                        {item.trabalho && <p className='entregar'> Entrega Trab.</p>}
+                    </div>
+                </div>
+            ))}
         </div>
     )
 }
