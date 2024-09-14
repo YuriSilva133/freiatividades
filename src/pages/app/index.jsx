1278312fd22438c2b1889ad79499120974702a72
Yuri Silva <yuriteofilo38@gmail.com>
2024-09-13 21:18:29 -0300

    Lista de objetos e mapeamento

--- src/pages/app/index.jsx
+++ src/pages/app/index.jsx
@@ -1,23 +1,51 @@
 import './index.scss'
 import Cabecalho from '../../components/cabecalho'
 
-import { useState } from 'react'
-
 //chamar api
-import axios from 'axios'
+//import axios from 'axios'
 
 export default function App() {
 
-    const [cards, setCards] = useState([])
-
-
+    /*
     //Chamar api
-    async function listarCards() {
-        const resposta = await axios.get('http://localhost:3001/cards')
-        console.log(resposta.data);
+    // async function listarCards() {
+    //     const resposta = await axios.get('http://localhost:3001/cards')
+    //     console.log(resposta.data);
 
-        setCards(resposta.data)
-    }
+    //     setCards(resposta.data)
+    // }
+    */
+
+    let listaObjetos = [
+        {
+            'status': 'completo',
+            'titulo': 'Componentes',
+            'descricao': '- Objetos e Listas de Objetos - Componentes',
+            'data': '26/agosto',
+            'trabalho': false
+        },
+        {
+            'status': 'em andamento',
+            'titulo': 'Renderização e Efeito',
+            'descricao': '- Componentes na renderização',
+            'data': '2/setembro',
+            'trabalho': false
+        },
+        {
+            'status': 'criado',
+            'titulo': 'Consumindo APIs - Parte 1',
+            'descricao': '- Componentes na renderização.',
+            'data': '9/setembro',
+            'trabalho': true
+        },
+        {
+            'status': 'evento',
+            'titulo': 'Componentes',
+            'descricao': '- Objetos e Listas de Objetos - Componentes',
+            'data': '16/setembro',
+            'trabalho': false
+        }
+    ]
 
     return (
         <div className='pagina-app'>
@@ -26,45 +54,19 @@ export default function App() {
             <div className='secao'>
                 <h1 className='titulo'> Conteúdos </h1>
 
-
                 <div className='cards'>
-
-                    <Card />
-
-                    {/*
-                    <Card
-                        status='completo'
-                        titulo='Componentes'
-                        descricao='- Objetos e Listas de Objetos - Componentes'
-                        data='26/agosto'
-                        trabalho={false}
-                    /> 
-                    
-                    <Card
-                        status='em andamento'
-                        titulo='Renderização e Efeito'
-                        descricao='- Componentes na renderização.'
-                        data='2/setembro'
-                        trabalho={false}
-                    />
-
-                    <Card
-                        status='criado'
-                        titulo='Consumindo APIs - Parte 1'
-                        descricao='- Componentes na renderização.'
-                        data='9/setembro'
-                        trabalho={true}
-                    />
-
-                    <Card
-                        status='evento'
-                        titulo='Componentes'
-                        descricao='- Objetos e Listas de Objetos - Componentes'
-                        data='16/setembro'
-                        trabalho={false}
-                    /> 
-                    */}
+                    {listaObjetos.map((item, index) => (
+                        <Card
+                            key={index}
+                            titulo={item.titulo}
+                            status={item.status}
+                            descricao={item.descricao}
+                            data={item.data}
+                            trabalho={item.trabalho}
+                        />
+                    ))}
                 </div>
+
             </div>
         </div>
     )
@@ -74,7 +76,6 @@ export function Card({ titulo, status, descricao, data, trabalho }) {
 
     let corPrimaria = '#A3E5BA'
     let corSegundaria = '#EFFBE2'
-
 
     switch (status) {
         case 'completo':
@@ -98,38 +99,18 @@ export function Card({ titulo, status, descricao, data, trabalho }) {
             break
 
         default:
-            //corSegundaria = '#F2F2F2'
+            corSegundaria = '#F2F2F2'
             break;
     }
 
-    /*
-    if (status === 'completo') {
-        corPrimaria = '#A3E5BA'
-        corSegundaria = '#EFFBE2'
-    }
-    else if (status === 'em andamento') {
-        corPrimaria = '#DCA3E5'
-        corSegundaria = '#EDE2FB'
-    }
-    else if (status === 'criado') {
-        corPrimaria = '#C3C3C3'
-        corSegundaria = '#F2F2F2'
-    }
-    else if (status === 'evento') {
-        corPrimaria = '#F6E448'
-        corSegundaria = '#FAF4C1'
-    }
-    */
-
     return (
         <div className='cards'>
-            {/* 
             <div className='card' style={{ backgroundColor: corSegundaria }}>
                 <div className='card-cabecalho' style={{ backgroundColor: corPrimaria }}>
                     <h2>{titulo}</h2>
                 </div>
-                <div className='card-conteudo'>
 
+                <div className='card-conteudo'>
                     {descricao &&
                         <div>
                             <h3>Assuntos</h3>
@@ -144,35 +125,7 @@ export function Card({ titulo, status, descricao, data, trabalho }) {
                     {data && <p className='tag'> {data} </p>}
                     {trabalho && <p className='entregar'> Entrega Trab.</p>}
                 </div>
-            </div> 
-            */}
-
-            {listaObjetos.map((item) => (
-
-                <div className='card' style={{ backgroundColor: corSegundaria}}>
-                    <div className='card-cabecalho' style={{ backgroundColor: corPrimaria }}>
-
-                        <h2>{item.titulo}</h2>
-                    </div>
-                    <div className='card-conteudo'>
-
-                        {item.descricao &&
-                            <div>
-                                <h3>Assuntos</h3>
-                                <pre>
-                                    {item.descricao}
-                                </pre>
-                            </div>
-                        }
-                    </div>
-
-                    <div className='card-tags'>
-                        {item.data && <p className='tag'> {item.data} </p>}
-                        {item.trabalho && <p className='entregar'> Entrega Trab.</p>}
-                    </div>
-                </div>
-
-            ))}
+            </div>
         </div>
     )
 }
